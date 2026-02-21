87f3e7dc7fb243a421aba9d2674b59768e9e59e7
PRCSM <173121089+PRCSM@users.noreply.github.com>
2026-02-21 08:52:46 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HYRUPApp from "../hyrup-v5 (1).jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HYRUPApp />
+  </React.StrictMode>
+);
